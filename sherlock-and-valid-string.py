68d42d4801483a9f9c6a4ce299693ba48fd47497
AMiT <theamit26@gmail.com>
2018-01-17 17:11:16 +0530

    Create sherlock-and-valid-string.py

---
+++ sherlock-and-valid-string.py
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem
+
+
+s =  input().strip()
+
+counter = [0]*26
+for i in range(len(s)):
+    counter[ord(s[i])-97]+=1
+count = 0
+counter = [x for x in counter if x != 0]
+countSet = set(counter)
+
+    
+from collections import Counter 
+
+Maxcount = Counter(counter).most_common()
+if len(Maxcount)==1:
+    print('YES')
+elif Maxcount[1][1] > 1 or len(Maxcount)>2 :
+    print('NO')
+else:
+    print('YES')
