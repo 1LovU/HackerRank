44bab030f1c943f6f7f6d46362b90ffeb479e37c
AMiT <theamit26@gmail.com>
2018-01-17 17:07:50 +0530

    Create nim_game.py

---
+++ nim_game.py
@@ -0,0 +1,21 @@
+$ https://www.hackerrank.com/challenges/misere-nim-1/problem
+
+g = int(input().strip())
+
+for _ in range(g):
+    n = int(input().strip())
+    s = [int(x) for x in input().strip().split(' ')]
+    x = 0
+    for i in s:
+        x ^= i
+    if len(set(s))==1 and 1 in s:
+        #here x=0 or 1
+        if x:#odd no. of ones
+            print("Second")
+        else:#even no. of ones
+            print("First")
+    else:
+        if x:
+            print("First")
+        else:
+            print("Second")
