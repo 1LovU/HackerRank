0ffab7edb7375d70650d865a92b70710c29f691a
AMiT <theamit26@gmail.com>
2018-01-17 19:10:36 +0530

    Create encryption.py

---
+++ encryption.py
@@ -0,0 +1,21 @@
+# https://www.hackerrank.com/challenges/encryption/problem
+
+import math
+
+
+s = list(input().strip())
+L =  len(s)
+row = math.floor(L**(1/2))
+col = math.ceil(L**(1/2))
+
+for i in range(col):
+    x = 0     
+    while True:
+        try:
+            print(s[i+x],end='')
+            x+= col
+        except:
+            break
+        
+    print(' ',end='')
+    
