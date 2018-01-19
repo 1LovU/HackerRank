9751f6057f25513e6cec1d9bf21a527b86fbd146
AMiT <theamit26@gmail.com>
2018-01-20 01:42:22 +0530

    Update jim-and-the-orders.py

--- jim-and-the-orders.py
+++ jim-and-the-orders.py
@@ -1,13 +1,14 @@
 # https://www.hackerrank.com/challenges/jim-and-the-orders/problem
 
 from collections import defaultdict
-from sys import stdin, stdout
+from sys import stdin
 
 my_dict = defaultdict(list)
 T = int(input())
-for i in range(T):
-    t,d = stdin.readline().split()
-    my_dict[t+d].append(i)
+for i in range(1,T+1):
+    t,d = map(int,stdin.readline().split())
+    my_dict[t+d].append(i)  
+
 for key in sorted(my_dict):
     print(*my_dict[key],end=' ')
 
